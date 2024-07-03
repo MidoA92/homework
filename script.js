dfae4f316aa676b325118fea0bba72d31cd152e4
midhat Ademovic <midhatademovic@hotmail.com>
2024-07-03 22:09:19 +0200

    init

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let niz= ['racunar', 'tastatura', 'mis'];
+//homeWork 1
+
+let firstArray= ['racunar', 'tastatura', 'mis'];
+
+//homework 2
+const shop= {
+    name: "MidoShop",
+    product: ['beardOil', 'beardCream', 'afterShave'],
+    openingHours: 9,
+    closingHours: 19
+};

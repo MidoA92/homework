97f7047c245d6d4180c8b231fe2793d30aad66bb
MidoA92 <87500895+MidoA92@users.noreply.github.com>
2024-07-05 05:43:10 +0200

    Add files via upload

---
+++ homework/script.js
@@ -0,0 +1,28 @@
+//homeWork 1
+
+let firstArray= ['racunar', 'tastatura', 'mis'];
+
+//homework 2
+const shop= {
+    name: "MidoShop",
+    product: ['beardOil', 'beardCream', 'afterShave'],
+    openingHours: 9,
+    closingHours: 19
+};
+
+
+//exercises
+ let names=[];
+ names.push('marija', 'petar', 'jovan');
+
+ console.log(names);
+ 
+ let komponente=['monitor', 'tastatura', 'mis'];
+ komponente.splice(0, komponente.length);
+ 
+ console.log(komponente);
+
+ let books=['Don Quixote', 'The Lord of the Rings', 'Harry Potter and the Philosopher’s Stone'];
+ books.unshift('Dream of the Red Chamber');
+
+ console.log(books);

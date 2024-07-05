5358ae66a0349a10b0ac3cc8f10c8a61f2468b77
midhat Ademovic <midhatademovic@hotmail.com>
2024-07-05 05:28:10 +0200

    init

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const shop= {
     openingHours: 9,
     closingHours: 19
 };
+
+
+//exercises
+ let names=[];
+ names.push('marija', 'petar', 'jovan');
+
+ console.log(names);
+ 
+ let komponente=['monitor', 'tastatura', 'mis'];
+ komponente.splice(0, komponente.length);
+ 
+ console.log(komponente);
+
+ let books=['Don Quixote', 'The Lord of the Rings', 'Harry Potter and the Philosopher’s Stone'];
+ books.unshift('Dream of the Red Chamber');
+
+ console.log(books);

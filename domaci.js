f955e220c4da3488d899b30351eca5017c1cc296
midhat Ademovic <midhatademovic@hotmail.com>
2024-07-10 01:14:31 +0200

    init

---
+++ domaci.js
@@ -0,0 +1,13 @@
+// homework 3
+
+let name= 'admin';
+let password= 'tajnasifra';
+
+
+if(name== 'admin'&& password=='tajnasifra'){
+    console.log('Dobr dosli nazad');
+} else if(name=='toma' && password=='tajnasifra'){
+    console.log('Dobro dosao Toma');
+} else{
+    console.log('Pogresna loznika ili ime');
+}

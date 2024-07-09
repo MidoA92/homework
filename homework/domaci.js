bac55b11f2a7b92f49d08977cd0b43c30ee20b30
MidoA92 <87500895+MidoA92@users.noreply.github.com>
2024-07-10 01:36:44 +0200

    Add files via upload

---
+++ homework/domaci.js
@@ -0,0 +1,42 @@
+// homework 3
+
+let name= 'admin';
+let password= 'tajnasifra';
+
+
+if(name== 'admin'&& password=='tajnasifra'){
+    console.log('Welcome back');
+} else if(name=='toma' && password=='tajnasifra'){
+    console.log('Welcome Toma');
+} else{
+    console.log('Wrong password or name');
+}
+
+
+// vjezba 3
+ let years = 30;
+
+ if(years<=18){
+    console.log('Minor');
+ } else{
+    console.log('Adult');
+ }
+
+ let score=[3, 10, 34, 45, 12, 32];
+
+ if(score[0]+ score[-1]> 50){
+    console.log('Score is greater than 50');
+ }else{
+    console.log('Score is less than 50');
+ }
+
+
+ let fruits= 'apple';
+
+ if(fruits== 'apple'){
+    console.log('apple');
+ } else if(fruits== banana){
+    console.log('banana');
+ } else{
+    console.log('Some other fruit');
+ }
